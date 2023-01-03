cee1357154fef6d6ac076ae33857d0f531198618
Marcelo <marcelo_thelin@hotmail.com>
2023-01-03 14:35:26 -0300

    Aulas 32 e 33

--- curso_udemy_lom/aula32.py
+++ curso_udemy_lom/aula32.py
@@ -3,3 +3,12 @@ Tipos built-in
 https://docs.python.org/3/library/stdtypes.html
 imutáveis já vistos: str, float, int, bool
 """
+
+string = 'Marcelo paiva'
+outra_variavel = string
+
+# string[3] = 'ABC' não funciona por ser imutavel
+# Precisaria ser feito assim:
+outra_variavel = f'{string[:4]}ABC{string[4:]}'
+
+print(outra_variavel.capitalize())
---
+++ curso_udemy_lom/aula33.py
@@ -0,0 +1,25 @@
+"""
+Repetições
+while (enquanto)
+Executa um bloco de código enquanto uma condição verdadeira
+O importante é que while precisa de uma situacao interna que torne a condicao False
+ou que interrompa o laço (break)
+de modo a não cair em um loop infinito
+"""
+"""
+condicao = True
+
+while condicao:
+    nome = input('Digite seu nome: ')
+    
+    if nome in 'sair':
+        break
+"""
+contador = 0
+
+while contador < 10:
+    print('EITA')
+    contador += 1
+    print(contador)
+
+print('Acabou')
