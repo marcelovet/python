0141250e9ec20f157e9b95c315c6b186bb68ecf7
Marcelo <marcelo_thelin@hotmail.com>
2023-01-03 16:54:19 -0300

    aulas 33 e 34

--- curso_udemy_lom/aula33.py
+++ curso_udemy_lom/aula33.py
@@ -23,3 +23,67 @@ while contador < 10:
     print(contador)
 
 print('Acabou')
+
+"""
+Operadores de atribuição com operadores aritmeticos
++= x, soma ou concatena x na variavel
+-= x, subtrai x na variavel
+*= x, multiplica x na variavel
+/= x, divide x na variavel
+//= x, divide inteiro x na variavel
+**= x, potencia x na variavel
+%= x, modulo x na variavel
+"""
+contador = 10
+
+while contador != 100:
+    print('EITA')
+    contador **= 2
+    print(contador)
+
+print('Acabou', end='\n\n------\n')
+
+"""
+continue - pula as linhas do bloco abaixo dele e reinicia a avaliação da condição do laço
+"""
+contador = 0
+
+while contador < 5:
+    contador += 1
+    
+    if contador == 3:
+        continue
+    
+    print(contador)
+
+print('Acabou')
+
+"""
+laços internos while dentro de while
+"""
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1    
+    while coluna <= qtd_colunas:
+        print(f'{linha=} - {coluna=}')
+        coluna += 1
+    linha += 1
+
+print('Acabou')
+
+"""
+iterando strings com while
+"""
+nome = 'Luiz Otavio'
+tamanho_nome = len(nome)
+nova_string = ''
+contador = 0
+
+while contador < tamanho_nome:
+    nova_string += f'{nome[contador]}*'
+    contador += 1
+
+print(nova_string)
---
+++ curso_udemy_lom/aula34.py
@@ -0,0 +1,50 @@
+"""
+Calculadora com while
+Pedir dois numeros e um operador
+e realizar a operação
+"""
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+
+    try:
+        numero_1_float = float(numero_1)
+        numero_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('Você digitou algo que não é um número')
+        continue
+
+    operadores_permitidos = '+-/*'
+    operador = input(f"""Digite a operação que quer realizar:
+    +: soma
+    -: subtração
+    /: divisão
+    *: multiplicação\nSeu operador: """)
+
+    while (operador not in operadores_permitidos) or not operador:
+        operador = input('Você utilizou uma operação inválida. Use um operador permitido!\nDigite seu operador: ')
+    while len(operador) > 1:
+        operador = input('Você utilizou uma operação inválida. Use somente um operador permitido!\nDigite seu operador: ')
+
+    if operador == '+':
+        resultado = numero_1_float + numero_2_float
+        print(f'{numero_1_float} + {numero_2_float} = {resultado:.2f}')
+    elif operador == '-':
+        resultado = numero_1_float - numero_2_float
+        print(f'{numero_1_float} - {numero_2_float} = {resultado:.2f}')
+    elif operador == '/':
+        resultado = numero_1_float / numero_2_float
+        print(f'{numero_1_float} / {numero_2_float} = {resultado:.2f}')
+    elif operador == '*':
+        resultado = numero_1_float * numero_2_float
+        print(f'{numero_1_float} * {numero_2_float} = {resultado:.2f}')
+    
+    # Opção de finalizar a calculadora
+    sair = input(f'{"-" * 100}\nQuer sair? [s]im: ')
+    sair = sair.lower().startswith('s')
+    if sair:
+        break
