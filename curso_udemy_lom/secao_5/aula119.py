97fd5558ae5159bff26162c9f2dedff4a3a492cf
Marcelo Paiva <marcelo_thelin@hotmail.com>
2023-01-18 17:30:22 -0300

    119

--- curso_udemy_lom/secao_5/aula119.py
+++ curso_udemy_lom/secao_5/aula119.py
@@ -3,5 +3,19 @@
 # @classmethod - cls, metodo de classe
 # @staticmethod - metodo estatico (X self, X cls)
 class Connection:
-    def __init__(self):
-        pass
+    def __init__(self, host='localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+
+    # se for definir algo de self, entao é metodo de instancia
+    def set_user(self, user):
+        self.user = user
+
+    def set_password(self, password):
+        self.user = password
+
+
+c1 = Connection()
+c1.set_user()
+c1.set_password()
