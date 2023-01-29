2a6b3911822ef9dac522a7a212ea9f0ba05bc77b
Marcelo Paiva <marcelo_thelin@hotmail.com>
2023-01-29 11:24:34 -0300

    155 a 157

---
+++ curso_udemy_lom/secao_5/aula157.py
@@ -0,0 +1,8 @@
+# Maria pegou um emprestimo de 1.000.000
+# para realizar o pagamento em 5 anos
+# A data em que ela pegou o emprestimo foi
+# 20/12/2020 e o vencimento de cada parcela é no dia
+# 20 de cada mes.
+# Crie a data do emprestimo
+# Cria a data do final do emprestimo
+# Mostre todas as datas de vencimento e o valor de cada parcela
